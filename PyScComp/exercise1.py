bf86d985726dfc837bbc4f8efe8099a7d8d1e366
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-08-02 04:26:44 +0300

    dawd

--- PyScComp/exercise1.py
+++ PyScComp/exercise1.py
@@ -1,8 +1,4 @@
-smallest = None
-print("Before: ", smallest)
-for itervar in [3, 41, 12, 9, 74, 15]:
-    if smallest is None or itervar < smallest:
-        smallest = itervar
-        break
-    print("Loop: ", itervar, smallest)
-print ("Smallest: ", smallest)
+counts = { 'quincy' : 1, 'mrugesh' : 42, 'beau' : 100, '0' : 10}
+print(counts)
+print(counts.get('kris', 0))
+print(counts)
