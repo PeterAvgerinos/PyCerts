25738b96fc0ea938a4bd7f442edb3d27f747feaf
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-08-25 23:12:27 +0300

    lolo

---
+++ PyScComp/exercises/exercise1.py
@@ -0,0 +1,12 @@
+counts = { 'quincy' : 1, 'mrugesh' : 42, 'beau' : 100, '0' : 10}
+print(counts)
+
+lst = []
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+lst = sorted(lst, reverse = True)
+print(lst)
+
+
+print(sorted([(v,k) for k,v in counts.keys()]))
