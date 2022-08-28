220f502dcf983ed43f3d9b0fd591a78f218e4e13
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-08-28 23:09:25 +0300

    lolo

--- PyScComp/objex/exercise1.py
+++ PyScComp/objex/exercise1.py
@@ -1,11 +1,16 @@
 class PartyAnimal:
     x = 0
+    name = ''
+    def __init__(self, nam):
+        self.name = nam
+        print(self.name, 'constructed')
     def party(self):
-        self.x = self.x + 2
-        print(self.x)
+        self.x = self.x + 1
+        print(self.name, 'party count', self.x)
 
-an = PartyAnimal()
-an.party()
-an.party()
-print('Type', type(an))
-print('Dir', dir(an))
+q = PartyAnimal('Quincy')
+m = PartyAnimal('Miya')
+
+q.party()
+m.party()
+q.party()
