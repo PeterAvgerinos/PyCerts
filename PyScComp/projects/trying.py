1dd7e85fa75d0c6959b1ed56827be95e46ca33f3
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-05 22:42:13 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -1,8 +1,19 @@
 import re
 
-problem = input("What\'s your problem dude?").replace(" ", "")
-matches = re.search("[0-9]+", problem)
+while True:
+    problem = input("What\'s your problem dude?").replace(" ", "")
+    matches = re.search(r"[0-9]+\+[0-9]+", problem)
 
-if matches:
+    if matches:
+        number1, number2 = int(matches[0]), int(matches[2])
+        operator = matches[1]
+        if operator == "+":
+            print(number1 + number2)
+            break
+        elif operator == "-":
+            print(number1 - number2)
+            break
+        else:
+            continue
 
 
