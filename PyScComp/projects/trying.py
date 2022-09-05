4b40e4103375767549e24eeb7815bc3252e0a609
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-05 22:43:20 +0300

    d

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -2,7 +2,7 @@ import re
 
 while True:
     problem = input("What\'s your problem dude?").replace(" ", "")
-    matches = re.search(r"[0-9]+\+[0-9]+", problem)
+    matches = re.search(r"[0-9]+|\+|[0-9]+", problem)
 
     if matches:
         number1, number2 = int(matches[0]), int(matches[2])
