33a5249545b8db8485c6d1da4d35fb77d3865c52
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-05 22:48:15 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -5,8 +5,8 @@ while True:
     matches = re.search(r"[0-9]+|\+|[0-9]+", problem)
 
     if matches:
-        number1, number2 = int(matches[0]), int(matches[2])
-        operator = matches[1]
+        number1, number2 = int(matches.group(0)), int(matches.group(2))
+        operator = matches.group(1)
         # if operator == "+":
         #     print(number1 + number2)
         #     break
