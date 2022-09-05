2719e72df975c040ba250d6805532c3149348a5a
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-05 22:44:24 +0300

    d

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -7,13 +7,15 @@ while True:
     if matches:
         number1, number2 = int(matches[0]), int(matches[2])
         operator = matches[1]
-        if operator == "+":
-            print(number1 + number2)
-            break
-        elif operator == "-":
-            print(number1 - number2)
-            break
-        else:
-            continue
+        # if operator == "+":
+        #     print(number1 + number2)
+        #     break
+        # elif operator == "-":
+        #     print(number1 - number2)
+        #     break
+        # else:
+        #     continue
+        print(matches[0], matches[1], matches[2])
+        break
 
 
