4d940f0c0fd8e55fdfc595c4a03309ded201a78d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 12:33:07 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -41,7 +41,7 @@ def arithmetic_arranger(problems, show_result = False):
         print(a[i][2], (1 - a[i][3])*" ", a[i][4], 5*" ", end="")
     print()
     for i in a:
-        print(5*"-", 5*" ", end="")
+        print(2*max(a[i][0], a[i][3])*"-", 5*" ", end="")
     print()
     for i in a:
         if show_result == True:
