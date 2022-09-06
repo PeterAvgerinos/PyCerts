581f77ab734da1c66000885f4f9a4a4210ee5fdb
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 17:33:24 +0300

    l]

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -4,43 +4,34 @@ list = ["399 + 554", "43 + 102", "25 - 200"]
 def arithmetic_arranger(problems, show_result = False):
     a = {}
 
-    try:
-        if len(list) > 5:
-            raise Exception("Error: Too many problems")
+    if len(list) > 5:
+        raise Exception("Error: Too many problems")
 
-        for item in problems:
-            problem = item.replace(" ", "")
-            matches = re.search(r"^([0-9]+)([\+|\-])([0-9]+)$", problem)
+    for item in problems:
+        problem = item.replace(" ", "")
+        matches = re.search(r"^([0-9]+)([\+|\-])([0-9]+)$", problem)
 
-            if matches:
-                number1, number2 = int(matches.group(1)), int(matches.group(3))
-                operator = matches.group(2)
-                if operator != "+" and operator != "-":
-                    raise Exception("Error: Operator must be + or -")
-                spaces1 = len(str(number1))
-                spaces2 = len(str(number2))
-                if spaces1 > 4 or spaces2 > 4:
-                    raise Exception("Error: Numbers can not be more than four digits")
-                if operator == "+":
-                    result = number1 + number2
-                    spacesr = len(str(result))
-                elif operator == "-":
-                    result = number1 - number2
-                    spacesr = len(str(result))
-                else:
-                    continue
-                new_value = {problem: [spaces1, number1, operator, spaces2, number2, spacesr, result]}
-                a.update(new_value)
+        if matches:
+            number1, number2 = int(matches.group(1)), int(matches.group(3))
+            operator = matches.group(2)
+            if operator != "+" and operator != "-":
+                raise Exception("Error: Operator must be + or -")
+            spaces1 = len(str(number1))
+            spaces2 = len(str(number2))
+            if spaces1 > 4 or spaces2 > 4:
+                raise Exception("Error: Numbers can not be more than four digits")
+            if operator == "+":
+                result = number1 + number2
+                spacesr = len(str(result))
+            elif operator == "-":
+                result = number1 - number2
+                spacesr = len(str(result))
             else:
-                raise AttributeError()
-    except KeyError:
-        print("Error: Too many problems")
-    except ImportError:
-        print("Error: Operator must be + or -")
-    except AttributeError:
-        print("Error: Only Numbers")
-    except ValueError:
-        print("Error: Numbers can not be more than four digits")
+                continue
+            new_value = {problem: [spaces1, number1, operator, spaces2, number2, spacesr, result]}
+            a.update(new_value)
+        else:
+            raise Exception("Error: Only Numbers")
 
     for i in a:
         print((5 - a[i][0])*" ", a[i][1], 5*" ", end="")
