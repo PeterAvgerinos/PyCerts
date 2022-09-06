45cca50829bc1f3901b387faa19a7f01808a9069
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 13:15:26 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -1,5 +1,5 @@
 import re
-list = ["399 - 554", "43 + 102", "25 - 200", "40 - 1", "1 - 2", "3 + 4"]
+list = ["399 - 554", "43 + 102", "25 - 200b"]
 
 def arithmetic_arranger(problems, show_result = False):
     a = {}
@@ -10,13 +10,17 @@ def arithmetic_arranger(problems, show_result = False):
 
         for item in problems:
             problem = item.replace(" ", "")
-            matches = re.search(r"([0-9]+)([\+|\-])([0-9]+)", problem)
+            matches = re.search(r"^([0-9]+)([\+|\-])([0-9]+)$", problem)
 
             if matches:
                 number1, number2 = int(matches.group(1)), int(matches.group(3))
                 operator = matches.group(2)
+                if operator != "+" and operator != "-":
+                    raise ArithmeticError()
                 spaces1 = len(str(number1))
                 spaces2 = len(str(number2))
+                if spaces1 > 4 or spaces2 > 4:
+                    raise ValueError()
                 if operator == "+":
                     result = number1 + number2
                     spacesr = len(str(result))
@@ -28,7 +32,12 @@ def arithmetic_arranger(problems, show_result = False):
                 new_value = {problem: [spaces1, number1, operator, spaces2, number2, spacesr, result]}
                 a.update(new_value)
     except KeyError:
-        print("Too many problems")
+        print("Error: Too many problems")
+    except ArithmeticError:
+        print("Error: Operator must be + or -")
+    except ValueError:
+        print("Error: Numbers can not be more than four digits")
+    
 
     for i in a:
         print((5 - a[i][0])*" ", a[i][1], 5*" ", end="")
