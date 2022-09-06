9a6fb06a634a0cf6dfb03b17bb417201b88fd55b
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 18:18:02 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -7,19 +7,21 @@ def arithmetic_arranger(problems, show_result = False):
     if len(problems) > 5:
         raise Exception("Error: Too many problems.")
 
-    for item in problems:
-        problem = item.replace(" ", "")
+    for problem in problems:
+        problem = problem.replace(" ", "")
         matches = re.search(r"^([0-9]+)([\+|\-])([0-9]+)$", problem)
+        if "+" or "-" not in problem:
+            raise Exception("Error: Operator must be \'+\' or \'-\'.")
 
         if matches:
             number1, number2 = int(matches.group(1)), int(matches.group(3))
             operator = matches.group(2)
             if operator != "+" and operator != "-":
-                raise Exception("Error: Operator must be + or -")
+                raise Exception("Error: Operator must be \'+\' or \'-\'.")
             spaces1 = len(str(number1))
             spaces2 = len(str(number2))
             if spaces1 > 4 or spaces2 > 4:
-                raise Exception("Error: Numbers can not be more than four digits")
+                raise Exception("Error: Numbers cannot be more than four digits.")
             if operator == "+":
                 result = number1 + number2
                 spacesr = len(str(result))
