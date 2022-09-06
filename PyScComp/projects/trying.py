490b4291fee8687e22dd6b1d253378994a976c79
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 12:46:57 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -1,30 +1,34 @@
 import re
-list = ["399 - 554", "43 + 102", "25 - 200"]
-liste = []
-
+list = ["399 - 554", "43 + 102", "25 - 200", "40 - 1", "1 - 2", "3 + 4"]
 
 def arithmetic_arranger(problems, show_result = False):
     a = {}
 
-    for item in problems:
-        problem = item.replace(" ", "")
-        matches = re.search(r"([0-9]+)([\+|\-])([0-9]+)", problem)
+    try:
+        if len(list) > 5:
+            raise KeyError()
 
-        if matches:
-            number1, number2 = int(matches.group(1)), int(matches.group(3))
-            operator = matches.group(2)
-            spaces1 = len(str(number1))
-            spaces2 = len(str(number2))
-            if operator == "+":
-                result = number1 + number2
-                spacesr = len(str(result))
-            elif operator == "-":
-                result = number1 - number2
-                spacesr = len(str(result))
-            else:
-                continue
-            new_value = {problem: [spaces1, number1, operator, spaces2, number2, spacesr, result]}
-            a.update(new_value)
+        for item in problems:
+            problem = item.replace(" ", "")
+            matches = re.search(r"([0-9]+)([\+|\-])([0-9]+)", problem)
+
+            if matches:
+                number1, number2 = int(matches.group(1)), int(matches.group(3))
+                operator = matches.group(2)
+                spaces1 = len(str(number1))
+                spaces2 = len(str(number2))
+                if operator == "+":
+                    result = number1 + number2
+                    spacesr = len(str(result))
+                elif operator == "-":
+                    result = number1 - number2
+                    spacesr = len(str(result))
+                else:
+                    continue
+                new_value = {problem: [spaces1, number1, operator, spaces2, number2, spacesr, result]}
+                a.update(new_value)
+    except KeyError:
+        print("Too many problems")
 
     for i in a:
         print((5 - a[i][0])*" ", a[i][1], 5*" ", end="")
