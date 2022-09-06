0b925612270c722a2cd5eeb50e134230e497c06c
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-06 11:42:44 +0300

    l

--- PyScComp/projects/trying.py
+++ PyScComp/projects/trying.py
@@ -4,31 +4,34 @@ liste = []
 
 
 def arithmetic_arranger(problems, show_result = False):
-    for item in problems:
-        problem = item.replace(" ", "")
-        matches = re.search(r"([0-9]+)([\+|\-])([0-9]+)", problem)
+    try:
+        for item in problems:
+            problem = item.replace(" ", "")
+            matches = re.search(r"([0-9]+)([\+|\-])([0-9]+)", problem)
 
-        if matches:
-            number1, number2 = int(matches.group(1)), int(matches.group(3))
-            operator = matches.group(2)
-            spaces1 = len(str(number1))
-            spaces2 = len(str(number2))
-            if operator == "+":
-                result = number1 + number2
-                spacesr = len(str(result))
-            elif operator == "-":
-                result = number1 - number2
-                spacesr = len(str(result))
+            if matches:
+                number1, number2 = int(matches.group(1)), int(matches.group(3))
+                operator = matches.group(2)
+                spaces1 = len(str(number1))
+                spaces2 = len(str(number2))
+                if operator == "+":
+                    result = number1 + number2
+                    spacesr = len(str(result))
+                elif operator == "-":
+                    result = number1 - number2
+                    spacesr = len(str(result))
+                else:
+                    continue
+                print((5 - spaces1)*" ",number1)
+                print(operator, (1 - spaces2)*" ", number2)
+                print(5*"-")
+                if show_result == True:
+                    print((5-spacesr)*" ", result)
+                print()
             else:
-                continue
-            print((5 - spaces1)*" ",number1)
-            print(operator, (1 - spaces2)*" ", number2)
-            print(5*"-")
-            if show_result == True:
-                print((5-spacesr)*" ", result)
-            print()
-        else:
-            ValueError("no problems found")
+                ValueError()
+    except ValueError:
+        print("no problems found")
 
 def main():
     return arithmetic_arranger(liste, True)
