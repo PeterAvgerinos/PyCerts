726e52ffc782263c3c5949bca8b10a45f140e2ef
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-10 23:25:56 +0300

    l

--- PyScComp/projects/time/timeismeaningless.py
+++ PyScComp/projects/time/timeismeaningless.py
@@ -1,42 +1,45 @@
 #AM before midday 1 - 12
 #PM after midday 12 - 24
-def add_time(start_time, duration):
+def add_time(start_time, duration, starting_day = None):
+    weekdays = {"Sunday" : 0, "Monday" : 1, "Tuesday" : 2, "Wednesday" : 3, "Thursday": 4, "Friday" : 5, "Saturday" : 6}
+    flag = "PM"
+
     start_time = start_time.strip(" ").upper()
-    print(start_time.capitalize())
+
     start_hour = int(start_time.split(":")[0])
     start_minutes = int(start_time.split(":")[1][:2])
     duration_hour = int(duration.split(":")[0])
     duration_minutes = int(duration.split(":")[1])
-    if "PM" in start_time:
+
+    if flag in start_time:
         start_hour = start_hour + 12
+
     new_hour = start_hour + duration_hour
-    print(new_hour)
     new_minutes = start_minutes + duration_minutes
-    print(new_minutes)
-    print("-------------")
+
     new_hour = new_hour + new_minutes//60
-    print(new_hour)
     new_minutes = new_minutes%60
-    print(new_minutes)
-    print("-------------")
+
     days = new_hour//24
-    print(days)
     new_hour = new_hour%24
 
-    days = new_hour//24
-    print(new_hour)
-    print(new_minutes)
-    # print(f"{new_hour}:{new_minutes} {days}")
+    if starting_day:
+        starting_day = starting_day.upper().title()
+        ending_day_value = weekdays[starting_day] + days%6
+        print(weekdays[f'{ending_day_value}'])
 
-
-
-    # print(start_time, start_hour, start_minute)
+    if days == 0 :
+        print(f"{new_hour}:{new_minutes}")
+    elif days == 1:
+        print(f"{new_hour}:{new_minutes} (next day)")
+    else:
+        print(f"{new_hour}:{new_minutes} ({days} days later)")
 
 def main():
     # add_time("3:00 PM", "3:10")
     # print("Should output 6:10 PM")
 
-    # print(add_time("11:30 PM", "2:30", "Monday"))
+    # add_time("11:30 AM", "2:32", "Monday")
     # print("Should output 2:02 PM, Monday")
     #
     # print(add_time("11:43 AM", "00:20"))
@@ -45,10 +48,10 @@ def main():
     # print(add_time("10:10 PM", "3:30"))
     # print("Should output 1:40 AM (next day)")
     #
-    # print(add_time("11:43 PM", "24:20", "tueSday"))
+    print(add_time("11:43 PM", "24:20", "tueSday"))
     # print("Should output 12:03 AM, Thursday (2 days later)")
     #
-    add_time("6:30 PM", "205:12")
+    # add_time("6:30 PM", "205:12", "Monday")
     # print("Should output 7:42 AM (9 days later)")
 
 if __name__ == "__main__":
