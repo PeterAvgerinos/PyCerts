b598f162554ce7f385f49a8fc02f483ef2541c7c
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-11 23:31:36 +0300

    l

--- PyScComp/projects/time/timeismeaningless.py
+++ PyScComp/projects/time/timeismeaningless.py
@@ -54,14 +54,14 @@ def main():
     # add_time("11:30 AM", "2:32", "Monday")
     # print("Should output 2:02 PM, Monday")
 
-    print(add_time("11:43 AM", "00:20"))
-    print("Should output 12:03 PM")
-
-    print(add_time("10:10 PM", "3:30"))
-    print("Should output 1:40 AM (next day)")
-
-    print(add_time("11:43 PM", "24:20", "tueSday"))
-    print("Should output 12:03 AM, Thursday (2 days later)")
+    add_time("11:43 AM", "00:20")
+    # print("Should output 12:03 PM")
+    #
+    # print(add_time("10:10 PM", "3:30"))
+    # print("Should output 1:40 AM (next day)")
+    #
+    # print(add_time("11:43 PM", "24:20", "tueSday"))
+    # print("Should output 12:03 AM, Thursday (2 days later)")
 
     # add_time("6:30 PM", "205:12")
     # print("Should output 7:42 AM (9 days later)")
