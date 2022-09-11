a4c89ac3c3ef3f7759fa501150d2b54cd76b5933
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-12 00:26:58 +0300

    k

--- PyScComp/projects/time/timeismeaningless2.py
+++ PyScComp/projects/time/timeismeaningless2.py
@@ -54,22 +54,22 @@ def add_time(start_time, duration, starting_day = None):
     return new_time
 
 def main():
-    add_time("3:00 PM", "3:10")
+    print(add_time("3:00 PM", "3:10"))
     print("Should output 6:10 PM")
 
-    add_time("11:30 AM", "2:32", "Monday")
+    print(add_time("11:30 AM", "2:32", "Monday"))
     print("Should output 2:02 PM, Monday")
 
-    add_time("11:43 AM", "00:20")
+    print(add_time("11:43 AM", "00:20"))
     print("Should output 12:03 PM")
 
-    add_time("10:10 PM", "3:30")
+    print(add_time("10:10 PM", "3:30"))
     print("Should output 1:40 AM (next day)")
 
-    add_time("11:43 PM", "24:20", "tueSday")
+    print(add_time("11:43 PM", "24:20", "tueSday"))
     print("Should output 12:03 AM, Thursday (2 days later)")
 
-    add_time("6:30 PM", "205:12")
+    print(add_time("6:30 PM", "205:12"))
     print("Should output 7:42 AM (9 days later)")
 
 if __name__ == "__main__":
