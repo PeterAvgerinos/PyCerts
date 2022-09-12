1c35a3e56973620dbcafbbb5cb3b09f73165a2c0
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-12 23:54:33 +0300

    l

--- PyScComp/projects/budget/budgetapp.py
+++ PyScComp/projects/budget/budgetapp.py
@@ -2,19 +2,30 @@ class Category:
     def __init__(self, name, ledger ):
         self.name = name
         self.ledger = ledger
-        total = 0
+        self.total = 0
 
     def deposit(self, amount, description):
         self.ledger.append({"amount" : amount, "description" : description})
+        self.total = self.total + amount
 
     def withdraw(self, amount, description):
-        for item in self.ledger:
-            self.total = self.total + self.ledger[item]
-            if self.total > abs(amount):
-                self.ledger.append({"amount" : amount, "description" : description})
-                return True
-            else:
-                return False
+        if self.total > amount:
+            self.total = self.total - amount
+            self.ledger.append({"amount" : amount, "description" : description})
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.total
+
+    def transfer(self, amount, Category):
+        if self.total > amount:
+            self.withdraw(amount, "Transfer to [Destination Budget Category]")
+            Category.deposit(amount, "Transfer from [Source Budget Category]")
+            return True
+        else:
+            return False
 
 
 
