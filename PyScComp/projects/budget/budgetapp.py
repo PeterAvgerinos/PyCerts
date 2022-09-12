2678a7aea5631d36846ee6e8e637b300d374f09c
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-12 23:46:42 +0300

    l

--- PyScComp/projects/budget/budgetapp.py
+++ PyScComp/projects/budget/budgetapp.py
@@ -2,12 +2,20 @@ class Category:
     def __init__(self, name, ledger ):
         self.name = name
         self.ledger = ledger
+        total = 0
 
     def deposit(self, amount, description):
         self.ledger.append({"amount" : amount, "description" : description})
 
     def withdraw(self, amount, description):
-        
+        for item in self.ledger:
+            self.total = self.total + self.ledger[item]
+            if self.total > abs(amount):
+                self.ledger.append({"amount" : amount, "description" : description})
+                return True
+            else:
+                return False
+
 
 
 
