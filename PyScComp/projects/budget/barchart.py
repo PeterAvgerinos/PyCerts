97bdba3c8b4c0b3fdb9868b602bff3131f42b8dd
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-13 23:17:12 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -1,6 +1,14 @@
 string = "Percentage spent by category \n"
-catperc = [{"name" : "Food", "Percentage" : 40, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0']},{"name" : "Auto", "Percentage" : 60, "listofos" : [' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0', '0', '0']},{"name" : "Clothing", "Percentage" : 20, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0']}]
+catperc = [{"name" : "Food", "Percentage" : 45, "listofos" : []},{"name" : "Auto", "Percentage" : 60, "listofos" : []},{"name" : "Clothing", "Percentage" : 20, "listofos" : []}]
 percentages = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
+for i in range(0, len(catperc)):
+    for j in range(0, len(percentages)):
+        if catperc[i]["Percentage"] > percentages[j]:
+            catperc[i]["listofos"].append("0")
+        else:
+            catperc[i]["listofos"].append(" ")
+
 
 for item in range(0, len(percentages)):
     string = string + f"{percentages[item]:>3}|"#  + " " + catperc[0]["listofos"][item] + " " + catperc[1]["listofos"][item] + " " + catperc[2]["listofos"][item] + "\n"
