3382a07cdec2e563559cc0eeed1900314848dcda
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-14 02:22:26 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -1,6 +1,13 @@
 string = "Percentage spent by category \n"
-catperc = [{"name" : "Food", "Percentage" : 45, "listofos" : []},{"name" : "Auto", "Percentage" : 60, "listofos" : []},{"name" : "Clothing", "Percentage" : 20, "listofos" : []}]
+catperc = [{"name" : "Food", "Percentage" : 45, "listofos" : [], "listofls" : []},{"name" : "Auto", "Percentage" : 60, "listofos" : [], "listofls" : []},{"name" : "Clothing", "Percentage" : 20, "listofos" : [], "listofls" : []}]
 percentages = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
+maximum = catperc[0]["name"]
+for k in range(0, len(catperc)):
+    if len(catperc[k]["name"]) > len(maximum):
+        maximum = catperc[k]["name"]
+
+print(maximum)
 
 for i in range(0, len(catperc)):
     for j in range(0, len(percentages)):
@@ -10,30 +17,11 @@ for i in range(0, len(catperc)):
             catperc[i]["listofos"].append(" ")
 
 for item in range(0, len(percentages)):
-    string = string + f"{percentages[item]:>3}|"#  + " " + catperc[0]["listofos"][item] + " " + catperc[1]["listofos"][item] + " " + catperc[2]["listofos"][item] + "\n"
+    string = string + f"{percentages[item]:>3}|"
     for jitem in range(0, len(catperc)):
         string = string + " " + catperc[jitem]["listofos"][item]
     string = string + "\n"
 string = string + "    ----------"
-
-maximum = catperc[0]["name"]
-for k in range(0, len(catperc)):
-    if len(catperc[k]["name"]) > len(maximum):
-        maximum = catperc[k]["name"]
-
-print(maximum)
-
-
-for i in ["string", "booldozer"]:
-    for k in range(max(len("string"), len("booldozer"))):
-        if bool(i[k]):
-            string = string + i[k]
-        else:
-            string = string + " "
-
-# for i in catperc:
-#     for k in i["name"]:
-#         print(catperc[i]["name"][k])
 
 print(string)
 
