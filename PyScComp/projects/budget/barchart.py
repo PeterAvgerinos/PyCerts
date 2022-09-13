767dd1596c2233fc7ed3cbab12810c19ee58998a
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-13 23:08:10 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -1,12 +1,11 @@
 string = "Percentage spent by category \n"
 catperc = [{"name" : "Food", "Percentage" : 40, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0']},{"name" : "Auto", "Percentage" : 60, "listofos" : [' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0', '0', '0']},{"name" : "Clothing", "Percentage" : 20, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0']}]
 percentages = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
 for item in range(0, len(percentages)):
     string = string + f"{percentages[item]:>3}|"#  + " " + catperc[0]["listofos"][item] + " " + catperc[1]["listofos"][item] + " " + catperc[2]["listofos"][item] + "\n"
     for jitem in range(0, len(catperc)):
         string = string + " " + catperc[jitem]["listofos"][item]
     string = string + "\n"
-#     for loco in range(0,11):
-#         string = string + catperc[0]["listofos"][loco] + catperc[1]["listofos"][loco] + catperc[2]["listofos"][loco] + "\n"
 string = string + "    ----------"
 print(string)
