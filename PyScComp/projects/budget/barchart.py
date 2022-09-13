5f1632ff06199916a68935c89b30cf159887ec6b
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-14 02:12:41 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -9,13 +9,31 @@ for i in range(0, len(catperc)):
         else:
             catperc[i]["listofos"].append(" ")
 
-
 for item in range(0, len(percentages)):
     string = string + f"{percentages[item]:>3}|"#  + " " + catperc[0]["listofos"][item] + " " + catperc[1]["listofos"][item] + " " + catperc[2]["listofos"][item] + "\n"
     for jitem in range(0, len(catperc)):
         string = string + " " + catperc[jitem]["listofos"][item]
     string = string + "\n"
 string = string + "    ----------"
+
+letters = []
+
+for k in range(0, len(catperc)):
+    letters.append(catperc[k]["name"])
+
+maximum = catperc[0]["name"]
+for k in range(0, len(catperc)):
+    if len(catperc[k]["name"]) > len(maximum):
+        maximum = catperc[k]["name"]
+
+for k in range(0, len(maximum)):
+    for j in range(0, len(catperc)):
+        if catperc[j]["name"][k]:
+            
+            letters[j] = letters[j] + (catperc[j]["name"][k])
+        else:
+            letters[j] = letters[j] + (catperc[j]["name"][k])
+print(letters)
 
 print(string)
 
