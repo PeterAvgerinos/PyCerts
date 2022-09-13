e9cdc52ba9c5c622d29aa3a09a619c4a4b74d51a
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-13 22:45:00 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -1,7 +1,5 @@
 string = "Percentage spent by category \n"
-catperc = [{"name" : "Food", "Percentage" : 40},{"name" : "Auto", "Percentage" : 60},{"name" : "Clothing", "Percentage" : 20}]
+catperc = [{"name" : "Food", "Percentage" : 40, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0']},{"name" : "Auto", "Percentage" : 60, "listofos" : [' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0', '0', '0']},{"name" : "Clothing", "Percentage" : 20, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0']}]
 percentages = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
 for item in percentages:
     string = string + f"{item:>4}|"
-    if catperc[item]["Percentage"] <= item:
-        print("o")
