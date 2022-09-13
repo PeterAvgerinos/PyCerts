1b46afc4b5f4bf46467360611616a1f975274522
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-13 03:46:37 +0300

    l

--- PyScComp/projects/budget/budgetapp.py
+++ PyScComp/projects/budget/budgetapp.py
@@ -27,10 +27,18 @@ class Category:
     def get_balance(self):
         return self.total
 
-    def transfer(self, amount, Category):
+    def get_description(self, i):
+        print(self.ledger[i]["description"])
+        return self.ledger[i]["description"]
+
+    def get_amount(self, i):
+        print(self.ledger[i]["amount"])
+        return self.ledger[i]["amount"]
+
+    def transfer(self, amount, category):
         if self.total > amount:
-            self.withdraw(amount, "Transfer to [Destination Budget Category]")
-            Category.deposit(amount, "Transfer from [Source Budget Category]")
+            self.withdraw(amount, "transfer to [destination budget category]")
+            category.deposit(amount, "transfer from [source budget category]")
             return True
         else:
             return False
@@ -41,13 +49,15 @@ class Category:
         else:
             return True
 
-food = Category("Food")
+food = Category("food")
 food.deposit(25, "poop")
 food.deposit(30, "piss")
 food.deposit(45, "ass")
 food.deposit(73, "dicks")
 food.withdraw(43, "pouch")
-print(food)
+food.get_amount(1)
+food.get_description(1)
+# print(food)
 
 
 def create_spend_chart(categories):
