658b8b21167ed00be2f2d71aeec10f52e509afd7
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-13 22:59:21 +0300

    l

--- PyScComp/projects/budget/barchart.py
+++ PyScComp/projects/budget/barchart.py
@@ -1,8 +1,8 @@
 string = "Percentage spent by category \n"
 catperc = [{"name" : "Food", "Percentage" : 40, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0']},{"name" : "Auto", "Percentage" : 60, "listofos" : [' ', ' ', ' ', ' ', ' ', '0', '0', '0', '0', '0', '0']},{"name" : "Clothing", "Percentage" : 20, "listofos" : [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0']}]
 percentages = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
-for item in percentages:
+for item in range(0, len(percentages)):
     string = string + f"{item:>4}|" + catperc[0]["listofos"][item]  + catperc[1]["listofos"][item] + catperc[2]["listofos"][item] + "\n"
 #     for loco in range(0,11):
 #         string = string + catperc[0]["listofos"][loco] + catperc[1]["listofos"][loco] + catperc[2]["listofos"][loco] + "\n"
-# print(string)
+print(string)
