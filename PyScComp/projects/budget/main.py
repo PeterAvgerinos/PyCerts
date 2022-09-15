bee06169cb75025a1d58358b769a6edebf41095e
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-15 17:33:26 +0300

    l

---
+++ PyScComp/projects/budget/main.py
@@ -0,0 +1,13 @@
+import budget
+from budget import create_spend_chart
+from unittest import main
+
+food = budget.Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = budget.Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+
