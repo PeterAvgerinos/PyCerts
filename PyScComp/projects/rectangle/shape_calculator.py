927b03214a4a80bfe06048d536b5064405f9cd06
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-16 15:47:00 +0300

    l

---
+++ PyScComp/projects/rectangle/shape_calculator.py
@@ -0,0 +1,46 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.heigth = height
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def get_area(self):
+        return self.width*self.height
+
+    def get_perimeter(self):
+        return 2*self.width + 2*self.height
+
+    def get_diagonal(self):
+        return ((self.width**2 + self.height**2)** .5)
+
+    def get_picture(self):
+        string = ""
+        for _ in range(0, self.height):
+            for _ in range(0, self.width):
+                string += "*"
+            string += "\n"
+        return string
+
+    def get_amount_inside(self, shape):
+        numbers = self.get_area()//shape.get_area()
+        return numbers
+
+class Square(Rectangle):
+    def __init__(self, side):
+        self.side = side
+        super().__init__(side, side)
+
+    def __str__(self):
+        return f"Square(side={self.side})"
+
+    def set_side(self, side):
+        self.side = side
+        super().__init__(side, side)
