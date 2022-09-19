141cbb812f4e4e3ba7e7968746f572cd04fc604d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-19 05:08:03 +0300

    l

--- PyScComp/projects/probcalc/prob_calculator.py
+++ PyScComp/projects/probcalc/prob_calculator.py
@@ -1,2 +1,9 @@
 class Hat:
-    pass
+    def __init__(self, **data):
+        self.contents = []
+        for item in data:
+            for _ in range(0, data[item]):
+                self.contents.append(list(data.values()).index(_))
+        print(self.contents)
+hat = Hat(black=6, red=4, green=3)
+print(hat)
