4dd33c10361ed2f60db951ae53ea1e4e78fce69f
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-23 19:08:57 +0300

    k

--- PyScComp/projects/probcalc/prob_calculator.py
+++ PyScComp/projects/probcalc/prob_calculator.py
@@ -30,7 +30,7 @@ class Hat:
                 j += 1
         return j == len(other.contents)
 
-def experiment(hat, num_balls_drawn, num_experiments, **expected_balls):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     m = 0
     hat2 = Hat(**expected_balls)
     for _ in range(0, num_experiments):
@@ -42,8 +42,6 @@ def experiment(hat, num_balls_drawn, num_experiments, **expected_balls):
     return probability
 
 hat = Hat(black=6, red=4, green=3)
-hat2 = Hat(purple=2, green=1)
-print(hat.contains(hat2))
 probability = experiment(hat, expected_balls={"red":2,"green":1}, num_balls_drawn=5, num_experiments=2000)
-
 print(probability)
+
