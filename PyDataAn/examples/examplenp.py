d3cf7f50fe311aa4bcb7a32312816499bf855d39
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:32:59 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib as plt
 
 b = np.array([[1,2,3,1,2,31,2,31,2,3],[2,3,41,2,31,2,31,2,31,2]], dtype='int32')
-print(b)
 a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(np.full_like(a, 100))
-print(a)
+c = np.random.randint(4, size = a.shape)
+print(c)
