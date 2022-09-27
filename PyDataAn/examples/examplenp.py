2278576e37d5a8600dd383f416203fc48948e1e0
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:23:42 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -3,3 +3,6 @@ import matplotlib as plt
 
 b = np.array([[1,2,3,1,2,31,2,31,2,3],[2,3,41,2,31,2,31,2,31,2]], dtype='int32')
 print(b)
+a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+a[:, 2] = 20
+print(a)
