6648bcf12a14c2fa67c09e7c12ad96dd0bb34a07
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:03:38 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib as plt
 
-b = np.array([[1.0,2.0,3.0],[3.0,4.0,5.0]])
-print(b)
+b = np.array([[1,2,3],[2,3,4]], dtype='int32')
+print(b.ndim)
+print(b.shape)
+print(b.dtype)
