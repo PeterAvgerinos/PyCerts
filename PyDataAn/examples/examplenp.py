8d6333c8f8f20bbf700a9d9ce004e4d800dc8222
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:28:16 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -4,5 +4,5 @@ import matplotlib as plt
 b = np.array([[1,2,3,1,2,31,2,31,2,3],[2,3,41,2,31,2,31,2,31,2]], dtype='int32')
 print(b)
 a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-a[:, 2] = 20
+print(np.full_like(a, 100))
 print(a)
