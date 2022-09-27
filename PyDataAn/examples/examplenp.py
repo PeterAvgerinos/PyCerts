c5ea9c67d2345c375a28ea6e4b86f6f3d5b2e1f9
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:35:39 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -3,5 +3,6 @@ import matplotlib as plt
 
 b = np.array([[1,2,3,1,2,31,2,31,2,3],[2,3,41,2,31,2,31,2,31,2]], dtype='int32')
 a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-c = np.random.randint(4, size = a.shape)
+c = np.identity(3)
+c = np.repeat(b, 3)
 print(c)
