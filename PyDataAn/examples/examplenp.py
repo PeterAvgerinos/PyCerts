0127a97cdc75563a1b3af390665caf242c97fa9d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:00:53 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -1,8 +1,5 @@
 import numpy as np
 import matplotlib as plt
 
-a = np.array([0,1,2,3,4,5])
-b = np.array([3,4,5,6,7,8])
-c = a*b
-print(c.argmax())
-print(c)
+b = np.array([[1.0,2.0,3.0],[3.0,4.0,5.0]])
+print(b)
