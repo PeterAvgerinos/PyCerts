0c548676cf19ba94a3b111939ff4ed191f8552b0
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-27 21:15:48 +0300

    l

--- PyDataAn/examples/examplenp.py
+++ PyDataAn/examples/examplenp.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib as plt
 
-b = np.array([[1,2,3],[2,3,4]], dtype='int32')
+b = np.array([[1,2,3,1,2,31,2,31,2,3],[2,3,41,2,31,2,31,2,31,2]], dtype='int32')
+print(b[1,2])
 print(b.ndim)
 print(b.shape)
 print(b.dtype)
