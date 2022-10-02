962642a5e42fc1bc352f8a3eaddc962c73bf442b
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-02 22:28:08 +0300

    l

--- PyDataAn/problems/problem1/mean_var_std.py
+++ PyDataAn/problems/problem1/mean_var_std.py
@@ -8,12 +8,12 @@ def calculate(list):
     data = np.array(list)
     data = data.reshape((3,3))
     calculations = {}
-    mean = []
-    variance = []
-    standard_deviation = []
-    maximum = []
-    minimum = []
-    total = []
+    mean = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
+    variance = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
+    standard_deviation = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
+    maximum = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
+    minimum = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
+    total = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
     calculations = {'mean' : mean,
             'variance' : variance,
             'standard_deviation' : standard_deviation,
