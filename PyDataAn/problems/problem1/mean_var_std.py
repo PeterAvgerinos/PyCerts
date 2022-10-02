8603eed4ca92fbfa5bec7245e136170d6bbf0193
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-02 22:02:30 +0300

    l

--- PyDataAn/problems/problem1/mean_var_std.py
+++ PyDataAn/problems/problem1/mean_var_std.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+list = [0,1,2,3,4,5,6,7,8]
+
 def calculate(list):
     data = np.array(list, [3,3])
     print(data)
+
+data = np.array(list)
+data = data.reshape((3,3))
+print(data)
