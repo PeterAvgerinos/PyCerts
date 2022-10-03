068f1668574c170179210833cf8d4211dc7d0f67
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-03 17:03:29 +0300

    l

--- PyDataAn/problems/problem1/mean_var_std.py
+++ PyDataAn/problems/problem1/mean_var_std.py
@@ -8,13 +8,12 @@ def calculate(list):
     data = np.array(list)
     data = data.reshape((3,3))
     calculations = {}
-    np.set_printoptions(precision=2)
     mean = [np.mean(data, axis=0), np.mean(data, axis=1), np.mean(data)]
     variance = [np.var(data, axis=0), np.var(data, axis=1), np.var(data)]
     standard_deviation = [np.std(data, axis=0), np.std(data, axis=1), np.std(data)]
     maximum = [np.max(data, axis=0), np.max(data, axis=1), np.max(data)]
     minimum = [np.min(data, axis=0), np.min(data, axis=1), np.min(data)]
-    total = [np.sum(data, axis=0, dtype=np.int32), np.sum(data, axis=1, dtype=np.int32), np.sum(data, dtype=np.int32)]
+    total = [np.sum(data, axis=0), np.sum(data, axis=1), np.sum(data)]
     calculations = {'mean' : mean,
             'variance' : variance,
             'standard_deviation' : standard_deviation,
