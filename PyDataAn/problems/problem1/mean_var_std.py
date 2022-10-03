346b6aec0d20577bee2a33d6110e6c881ffe7a39
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-03 17:01:45 +0300

    l

--- PyDataAn/problems/problem1/mean_var_std.py
+++ PyDataAn/problems/problem1/mean_var_std.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-list = [0,1,2,3,4,5,6,7,8]
+list = [0,2,2,3,4,5,6,7,8]
 
 def calculate(list):
     if len(list) != 9:
@@ -8,9 +8,10 @@ def calculate(list):
     data = np.array(list)
     data = data.reshape((3,3))
     calculations = {}
-    mean = [np.mean(data, axis=0, dtype=np.float32), np.mean(data, axis=1, dtype=np.float32), np.mean(data, dtype=np.float32)]
-    variance = [np.var(data, axis=0, dtype=np.float32), np.var(data, axis=1, dtype=np.float32), np.var(data, dtype=np.float32)]
-    standard_deviation = [np.std(data, axis=0, dtype=np.float32), np.std(data, axis=1, dtype=np.float32), np.std(data, dtype=np.float32)]
+    np.set_printoptions(precision=2)
+    mean = [np.mean(data, axis=0), np.mean(data, axis=1), np.mean(data)]
+    variance = [np.var(data, axis=0), np.var(data, axis=1), np.var(data)]
+    standard_deviation = [np.std(data, axis=0), np.std(data, axis=1), np.std(data)]
     maximum = [np.max(data, axis=0), np.max(data, axis=1), np.max(data)]
     minimum = [np.min(data, axis=0), np.min(data, axis=1), np.min(data)]
     total = [np.sum(data, axis=0, dtype=np.int32), np.sum(data, axis=1, dtype=np.int32), np.sum(data, dtype=np.int32)]
