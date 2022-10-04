d7cc4fe09077e1bb3929d1df82ab4a38004602a3
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-04 14:37:08 +0300

    l

--- PyDataAn/problems/problem2/demographic_data_analyzer.py
+++ PyDataAn/problems/problem2/demographic_data_analyzer.py
@@ -4,10 +4,9 @@ import pandas as pd
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
-    print(df.head(5))
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].unique().count()
 
     # What is the average age of men?
     average_age_men = None
