0dc70c85966679256bdc89875db230160560644c
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-16 22:37:33 +0300

    l

--- Networking/examples/example1.py
+++ Networking/examples/example1.py
@@ -2,8 +2,10 @@ import socket
 
 #Gets private IP address
 host = socket.gethostbyname(socket.gethostname())
+port = 9090
 HOST = ''
 
 #TCP Socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(host)
 
