2650b83dccb78b36e1fc0ba7c86d4af5ca21720b
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-16 22:43:04 +0300

    l

--- Networking/examples/example1.py
+++ Networking/examples/example1.py
@@ -12,4 +12,10 @@ server.listen(5)
 
 while True:
     communication_socket, address = server.accept()
+    print(f'Connected to {address}')
+    message = communication_socket.recv(1024).decode('utf-8')
+    print(f'Message from client is {message}')
+    communication_socket.send(f'Got your message'.encode('utf-8'))
+    communication_socket.close()
+    print(f'Connection with {address} ended!')
 
