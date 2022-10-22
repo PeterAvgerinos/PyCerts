7b4fcb382cfbc772be5437a29a4dd7d31d67157f
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-22 14:08:59 +0300

    l

--- PyDataAn/MLNTUA/poop.cpp
+++ PyDataAn/MLNTUA/poop.cpp
@@ -1,7 +1,7 @@
 const int *array;
-for (int i=0; i<5; i++) { 
+for (const auto & i=0; i<5; i++) { 
     array[i] = i;
-}
-for (int i=0; i<1000; i++) { 
+};
+for (const auto & i=0; i<1000; i++) { 
     array[i] = array[i]*8;
-}
+};
