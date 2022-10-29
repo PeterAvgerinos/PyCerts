1601716c25269007ddc9eec88b2fc193daa9c56d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-30 00:25:29 +0300

    l

--- Visualization/Mechanix/vis/mecanix/board.py
+++ Visualization/Mechanix/vis/mecanix/board.py
@@ -22,9 +22,6 @@ class Board(pygame.sprite.Sprite):
         for square in range(11):
             pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 5)*SQUARE_SIZE, SQUARE_SIZE*5 + 50, SQUARE_SIZE, SQUARE_SIZE))
 
-        # for i in range(6):
-        #     for r in range(1, 13, 2):
-        #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (r-i)*SQUARE_SIZE, SQUARE_SIZE*i + 50, SQUARE_SIZE, SQUARE_SIZE))
 
     def draw_gear_mounts(self, win):
         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 + 2*MOUNT_SIZE, 50 + 2*MOUNT_SIZE), MOUNT_SIZE)
@@ -42,17 +39,8 @@ class Board(pygame.sprite.Sprite):
     def create_board(self):
         for row in range(6):
             self.board.append([])
-            if row == 0:
-                for col in range(row + 1):
-                    self.board[row].append(Gear(row, col, 'green', 20, 20))
-            elif row == 1:
-                for col in range(row + 2):
-                    self.board[row].append(Gear(row, col, 'green', 20, 20))
-            else:
-                for col in range(row + 3):
-                    self.board[row].append(Gear(row, col, 'green', 20, 20))
-                
-
+            for col in range(row*2 + 1):
+                self.board[row].append(Gear(row, col, 'green', 20, 20))
 
 board = Board()
 board.create_board()
@@ -60,9 +48,4 @@ print(board.board)
 board.board[1][1].make_jammed()
 print(board.board)
 
-    # def draw_squares(self, win):
-    #     win.fill(BLACK)
-    #     for row in range(ROWS):
-    #         for col in range(row % 2, ROWS, 2):
-    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
