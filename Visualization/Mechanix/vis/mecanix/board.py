8736ef557e48ed3a7b8d9960afb947d0535cdde7
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-29 22:22:42 +0300

    l

---
+++ Visualization/Mechanix/vis/mecanix/board.py
@@ -0,0 +1,51 @@
+import pygame
+from .constants import *
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_left = self.blue_left = 12
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2, 50, SQUARE_SIZE, SQUARE_SIZE))
+        for square in range(3):
+            pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 1)*SQUARE_SIZE, SQUARE_SIZE + 50, SQUARE_SIZE, SQUARE_SIZE))
+        for square in range(5):
+            pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 2)*SQUARE_SIZE, SQUARE_SIZE*2 + 50, SQUARE_SIZE, SQUARE_SIZE))
+        for square in range(7):
+            pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 3)*SQUARE_SIZE, SQUARE_SIZE*3 + 50, SQUARE_SIZE, SQUARE_SIZE))
+        for square in range(9):
+            pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 4)*SQUARE_SIZE, SQUARE_SIZE*4 + 50, SQUARE_SIZE, SQUARE_SIZE))
+        for square in range(11):
+            pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 5)*SQUARE_SIZE, SQUARE_SIZE*5 + 50, SQUARE_SIZE, SQUARE_SIZE))
+
+        # for i in range(6):
+        #     for r in range(1, 13, 2):
+        #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (r-i)*SQUARE_SIZE, SQUARE_SIZE*i + 50, SQUARE_SIZE, SQUARE_SIZE))
+
+    def draw_gear_mounts(self, win):
+        pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 + 2*MOUNT_SIZE, 50 + 2*MOUNT_SIZE), MOUNT_SIZE)
+        for square in range(3):
+            pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 1)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
+        for square in range(5):
+            pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 2)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*2+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
+        for square in range(7):
+            pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 3)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*3+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
+        for square in range(9):
+            pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 4)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*4+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
+        for square in range(11):
+            pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 5)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*5+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
+
+    def create_board(self):
+        pass
+
+
+
+    # def draw_squares(self, win):
+    #     win.fill(BLACK)
+    #     for row in range(ROWS):
+    #         for col in range(row % 2, ROWS, 2):
+    #             pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
---
+++ Visualization/Mechanix/vis/mecanix/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+WIDTH, HEIGHT = 1200, 800
+ROWS, COLS = 8,8
+SQUARE_SIZE = HEIGHT//COLS
+MOUNT_SIZE = SQUARE_SIZE//4
+
+#RGB Colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
