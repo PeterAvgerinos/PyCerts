0152a7c8c3728889127e18806104245f420d16ce
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-06 16:27:45 +0200

    l

--- Visualization/Mechanix/vis/mecanix/board.py
+++ Visualization/Mechanix/vis/mecanix/board.py
@@ -35,34 +35,3 @@ class Board(pygame.sprite.Sprite):
                     new_gear = Gear(row, col, 'transparent', WIDTH//2 - SQUARE_SIZE//2 - (row - col)*SQUARE_SIZE + 2*MOUNT_SIZE,  SQUARE_SIZE*row + 2*MOUNT_SIZE + 50)
                 self.board[row].append(new_gear)
                 new_gear.draw(win)
-
-    # def draw_squares(self, win):
-    #     win.fill(BLACK)
-    #     pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2, 50, SQUARE_SIZE, SQUARE_SIZE))
-    #     for square in range(3):
-    #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 1)*SQUARE_SIZE, SQUARE_SIZE + 50, SQUARE_SIZE, SQUARE_SIZE))
-    #     for square in range(5):
-    #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 2)*SQUARE_SIZE, SQUARE_SIZE*2 + 50, SQUARE_SIZE, SQUARE_SIZE))
-    #     for square in range(7):
-    #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 3)*SQUARE_SIZE, SQUARE_SIZE*3 + 50, SQUARE_SIZE, SQUARE_SIZE))
-    #     for square in range(9):
-    #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 4)*SQUARE_SIZE, SQUARE_SIZE*4 + 50, SQUARE_SIZE, SQUARE_SIZE))
-    #     for square in range(11):
-    #         pygame.draw.rect(win, RED, (WIDTH//2 - SQUARE_SIZE//2 - (square - 5)*SQUARE_SIZE, SQUARE_SIZE*5 + 50, SQUARE_SIZE, SQUARE_SIZE))
-
-
-    # def draw_gear_mounts(self, win):
-    #     pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 + 2*MOUNT_SIZE, 50 + 2*MOUNT_SIZE), MOUNT_SIZE)
-    #     for square in range(3):
-    #         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 1)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
-    #     for square in range(5):
-    #         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 2)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*2+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
-    #     for square in range(7):
-    #         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 3)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*3+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
-    #     for square in range(9):
-    #         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 4)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*4+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
-    #     for square in range(11):
-    #         pygame.draw.circle(win, BLACK, (WIDTH//2 - SQUARE_SIZE//2 - (square - 5)*SQUARE_SIZE+ 2*MOUNT_SIZE, SQUARE_SIZE*5+ 2*MOUNT_SIZE + 50), MOUNT_SIZE)
-
-
-
