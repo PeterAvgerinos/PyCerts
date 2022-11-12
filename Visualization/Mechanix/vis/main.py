01dfdd5d5fc18b1bca42a440c3e942647ee63e52
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-12 22:34:56 +0200

    k

--- Visualization/Mechanix/vis/main.py
+++ Visualization/Mechanix/vis/main.py
@@ -12,9 +12,43 @@ pygame.display.set_caption('Mecanix: The Gear Game')
 
 def get_pos_from_mouse(pos):
     x, y = pos
-    row = x*SQUARE_SIZE
-    col = y*SQUARE_SIZE
-    return col, row
+    if (660 <= x <= 739) & (53 <= y <= 131):
+        row, col = 0, 0
+        return col, row
+    elif (134 <= y <= 211):
+        if (621 <= x <= 699):
+            row, col = 1,0
+            return col, row
+        elif (702 <= x <= 777):
+            row, col = 1,1
+            return col, row
+    elif (215 <= y <= 290):
+        if (581 <= x <= 658):
+            row, col = 2,0
+            return col, row
+        elif (663 <= x <= 737):
+            row, col = 2,1
+            return col, row
+        elif (745 <= x <= 817):
+            row, col = 2,2
+            return col, row
+    elif (295 <= y <= 369):
+        if (541 <= x <= 618):
+            row, col = 3,0
+            return col, row
+        elif (622 <= x <= 696):
+            row, col = 3,1
+            return col, row
+        elif (703 <= x <=774):
+            row, col = 3,2
+            return col, row
+        elif (785 <= x <= 857):
+            row, col = 3,3
+            return col, row
+    else:
+        row = x*SQUARE_SIZE
+        col = y*SQUARE_SIZE
+        return col, row
 
 def main():
     run = True
@@ -22,10 +56,6 @@ def main():
     # game = Game(WIN)
     board = Board()
     board.create_board(WIN)
-    board.move(board.get_gear(3,2), 'green', WIN)
-    board.move(board.get_gear(4,2), 'green', WIN)
-    board.move(board.get_gear(5,2), 'green', WIN)
-    board.move(board.get_gear(6,2), 'green', WIN)
 
     while run:
         clock.tick(FPS)
@@ -40,7 +70,7 @@ def main():
                     print(pos)
                     row, col = get_pos_from_mouse(pos)
                     gear = board.get_gear(row, col)
-                    print(gear.x, gear.y)
+                    # print(gear.x, gear.y)
                     board.move(gear, 'green', WIN)
                 except TypeError:
                     pass
--- Visualization/Mechanix/vis/mecanix/constants.py
+++ Visualization/Mechanix/vis/mecanix/constants.py
@@ -17,3 +17,35 @@ BLUEGEAR = pygame.image.load("assets/blue-gear-md.png")
 GREENGEAR = pygame.image.load("assets/imagination-movers-gears-md.png")
 TARGETGEAR = pygame.image.load("assets/green-gear.svg.med.png")
 TRANSPARENT = pygame.image.load("assets/block.png")
+
+#Functions
+def get_pos_from_mouse(pos):
+    x, y = pos
+    if (660 <= x <= 739) & (53 <= y <= 131):
+        row, col = 0, 0
+    elif (134 <= y <= 211):
+        if (621 <= x <= 699):
+            row, col = 1,0
+        elif (702 <= x <= 777):
+            row, col = 1,1
+    elif (215 <= y <= 290):
+        if (581 <= x <= 658):
+            row, col = 2,0
+        elif (663 <= x <= 737):
+            row, col = 2,1
+        elif (745 <= x <= 817):
+            row, col = 2,2
+    elif (295 <= y <= 369):
+        if (541 <= x <= 618):
+            row, col = 3,0
+        elif (622 <= x <= 696):
+            row, col = 3,1
+        elif (703 <= x <=774):
+            row, col = 3,2
+        elif (785 <= x <= 857):
+            row, col = 3,3
+        pass
+
+    row = x*SQUARE_SIZE
+    col = y*SQUARE_SIZE
+    return col, row
