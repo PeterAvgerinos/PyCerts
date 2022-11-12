79873be012dc0c3a39a293fb93f14a84f88ddd04
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-12 19:17:02 +0200

    l

--- Visualization/Mechanix/vis/mecanix/game.py
+++ Visualization/Mechanix/vis/mecanix/game.py
@@ -8,13 +8,31 @@ class Game:
 
     def _init(self):
         self.selected = None
-        self.board = Board(self.win)
+        self.board = Board()
         self.turn = 'blue'
         self.valid_moves = {}
 
     def update(self):
-        self.board.update(self.win)
+        self.board.update_board(self.win)
         pygame.display.update()
 
     def reset(self):
         self._init()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self.move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+        else:
+            gear = self.board.get_gear(row, col)
+            if gear != None and gear.color == self.turn:
+                self.selected = gear
+                self.valid_moves = self.board.get_valid_moves(gear)
+                return True
+        return False
+
+
+    def move(self, row, col):
+        pass
