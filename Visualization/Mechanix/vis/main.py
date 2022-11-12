fda65b8e00179289aaa02b3979ba5aeb3112216b
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-12 21:51:04 +0200

    l

--- Visualization/Mechanix/vis/main.py
+++ Visualization/Mechanix/vis/main.py
@@ -12,8 +12,8 @@ pygame.display.set_caption('Mecanix: The Gear Game')
 
 def get_pos_from_mouse(pos):
     x, y = pos
-    row = (x - 300)//SQUARE_SIZE
-    col = (y  )//SQUARE_SIZE
+    row = x*SQUARE_SIZE
+    col = y*SQUARE_SIZE
     return col, row
 
 def main():
@@ -26,7 +26,6 @@ def main():
     board.move(board.get_gear(4,2), 'green', WIN)
     board.move(board.get_gear(5,2), 'green', WIN)
     board.move(board.get_gear(6,2), 'green', WIN)
-    print(board)
 
     while run:
         clock.tick(FPS)
