bbde3ba383bd9550f43a5d53d3f43d7b4ce5c5df
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-11-12 23:17:44 +0200

    l

--- Visualization/Mechanix/vis/main.py
+++ Visualization/Mechanix/vis/main.py
@@ -8,12 +8,22 @@ pygame.init()
 
 FPS = 60
 
+def convert_pos(ROWS, board):
+    ROW, COL = 0,0
+    m_x, m_y = pygame.mouse.get_pos()
+    for row in range(ROWS):
+        for col in range(row + 1):
+            center_x, center_y = board.get_gear(row, col).x, board.get_gear(row, col).y
+            dis = math.sqrt((center_x - m_x)**2 + (center_y - m_y)**2)
+            if dis < SQUARE_SIZE//2:
+                ROW, COL = row, col
+    print(ROW, COL)
+    return ROW, COL
+
+
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Mecanix: The Gear Game')
-
-def get_pos_from_mouse(pos):
-    a, b = pos
-
 
 def main():
     run = True
@@ -31,16 +41,8 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 try:
-                    ROW, COL = 0,0
-                    m_x, m_y = pygame.mouse.get_pos()
-                    for row in range(ROWS):
-                        for col in range(row + 1):
-                            center_x, center_y = board.get_gear(row, col).x, board.get_gear(row, col).y
-                            dis = math.sqrt((center_x - m_x)**2 + (center_y - m_y)**2)
-                            if dis < SQUARE_SIZE//2:
-                                ROW, COL = row, col
-                    print(ROW, COL)
-                    gear = board.get_gear(ROW, COL)
+                    row, col = convert_pos(ROWS, board)
+                    gear = board.get_gear(row, col)
                     # print(gear.x, gear.y)
                     board.move(gear, 'green', WIN)
                 except TypeError:
